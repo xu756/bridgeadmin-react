a66d3dd4601df3885367420ce1fe897cb75e7121
xu756 <756334744@qq.com>
2023-04-15 10:30:28 +0800

    rem

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,28 @@
 import Index from "./index";
 import "../assets/global.scss"
 import 'antd/dist/reset.css';
+import {useEffect} from "react";
+
 export default function App() {
+    const resizeListener = () => {
+        // 定义设计图的尺寸 3840
+        let designSize = 1920;
+        // 获取 html 元素
+        let html = document.documentElement;
+        // 定义窗口的宽度
+        let clientW = html.clientWidth;
+        // html 的fontsize 大小
+        let htmlRem = (clientW * 16) / designSize;1
+        html.style.fontSize = htmlRem + 'px';
+        // console.log(clientW);
+    }
+    useEffect(() => {
+        resizeListener();
+        window.addEventListener('resize', resizeListener);
+        return () => {
+            window.removeEventListener('resize', resizeListener);
+        }
+    })
     return (
         Index()
     );
