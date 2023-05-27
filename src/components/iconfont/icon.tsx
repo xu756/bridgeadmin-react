0167a6ece7f4073a0b27871f1ced3c04d2b718c2
xu756 <756334744@qq.com>
2023-05-27 11:26:49 +0800

    message

--- src/components/iconfont/icon.tsx
+++ src/components/iconfont/icon.tsx
@@ -1,6 +1,6 @@
 import {createFromIconfontCN} from '@ant-design/icons';
 
-export const IconUrl: string = "//at.alicdn.com/t/c/font_4078039_0dr6ug8an79v.js";
+export const IconUrl: string = "//at.alicdn.com/t/c/font_4078039_mgoiuwhmr0o.js";
 const IconFont = createFromIconfontCN({
     scriptUrl: IconUrl,
 });
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,14 +1,16 @@
 import "./login.scss";
-import {Button, Col, Row} from 'antd';
-import {useDispatch} from 'react-redux';
+import {Button, Checkbox, Col, Form, Input, Row,message} from 'antd';
 import {setUser} from "@/store/user";
 import {useSelector} from 'react-redux';
-import {RootState} from '@/store';
+import store, {RootState} from '@/store';
 import {useEffect} from "react";
 import {NoAuthApi} from "@/utils/api";
+import IconFont from "@/components/iconfont/icon";
 
 export default () => {
     const api = new NoAuthApi();
+    const [messageApi, contextHolder] = message.useMessage();
+
     const user = useSelector((state: RootState) => state.User);
     useEffect(() => {
         // init();
@@ -20,13 +22,56 @@ export default () => {
             console.log(e);
         });
     }
-    const dispatch = useDispatch();
+    const Login = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const LoginFailed = (errorInfo: any) => {
+        messageApi.error("请输入用户名和密码");
+    };
     return (
         <div className="login">
-            <div className="login_box">
-                <Row className="w100" >
-                    <Col className="login_box_left" span={12}></Col>
-                    <Col className="login_box_right" span={12}></Col>
+            {contextHolder}
+            <div className="login-box">
+                <Row className="w100">
+                    <Col className="login-box-left" span={12}>
+                        1
+                    </Col>
+                    <Col className="login-box-right" span={12}>
+                        <Form onFinish={Login}
+                              onFinishFailed={LoginFailed}
+                        >
+                            <Form.Item
+                                name={"username"}
+                                rules={[{required: true, message: '请输入用户名'}]}
+                            >
+                                <Input prefix={<IconFont type={"yg-icon-yonghu"}/>} placeholder="手机号"/>
+                            </Form.Item>
+                            <Form.Item
+                                name={"password"}
+                                rules={[{required: true, message: '请输入密码'}]}
+                            >
+                                <Input.Password prefix={<IconFont type={"yg-icon-tianchongxing-"}/>} placeholder="密码"/>
+                            </Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Row justify={"space-between"}>
+                                    <Col span={8}>
+                                        <Checkbox>记住密码</Checkbox>
+                                    </Col>
+                                    <Col span={8} className="login-form-forgot">
+                                        <a href="">
+                                            忘记密码
+                                        </a>
+                                    </Col>
+                                </Row>
+                            </Form.Item>
+                            <Form.Item className="login-form—submit">
+                                <Button type="primary" htmlType="submit" className="login-form—submit-button">
+                                    登录
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
                 </Row>
             </div>
         </div>
