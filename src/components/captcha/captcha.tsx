ee5a4ff407010f1f3e57678ffafcda803f8cde28
xu756 <756334744@qq.com>
2023-06-02 01:26:03 +0800

    验证码 点

--- src/components/captcha/captcha.tsx
+++ src/components/captcha/captcha.tsx
@@ -1,41 +1,80 @@
-import {CaptchaRes} from "@/model/Api";
+import { CaptchaRes } from "@/model/Api";
 import IconFont from "@/components/iconfont/icon";
-import {Button} from "antd";
+import { Button } from "antd";
+import React, { useState } from "react";
 
 interface CaptchaValue {
-    value: CaptchaRes
-    width?: string
-    height?: string
+    value: CaptchaRes;
+    width?: string;
+    height?: string;
+}
 
+interface Dot {
+    x: number;
+    y: number;
+    index: number;
 }
 
 export default (prop: CaptchaValue) => {
+    const [dots, setDots] = useState<Dot[]>([]);
+
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const { offsetX, offsetY } = event.nativeEvent;
+        const newDot: Dot = {
+            x: offsetX,
+            y: offsetY,
+            index: dots.length + 1,
+        };
+        setDots([...dots, newDot]);
+    };
 
     return (
-        <div className={"captcha"} style={{
-            width: '300px' || prop.width,
-        }}>
-            <div className={"captcha-header"}>
-                <div className={"captcha-header-title"}>验证码验证</div>
-                <div className={"captcha-header-thumb"} style={{
-                    backgroundImage: `url(${prop.value.thumb})`
-                }}></div>
+        <div
+            className="captcha"
+            style={{
+                width: "300px" || prop.width,
+            }}
+        >
+            <div className="captcha-header">
+                <div className="captcha-header-title">验证码验证</div>
+                <div
+                    className="captcha-header-thumb"
+                    style={{
+                        backgroundImage: `url(${prop.value.thumb})`,
+                    }}
+                ></div>
             </div>
-            <div className={"captcha-body"} style={{
-                height: '236px' || prop.height,
-                backgroundImage: `url(${prop.value.image})`
-            }}></div>
-            <div className={"captcha-footer"}>
-                <div className={"captcha-footer-icon"}>
-                    <div className={"captcha-footer-close"}>
-                        <IconFont type={"yg-icon-quxiao"}/>
+            <div
+                className="captcha-body"
+                style={{
+                    height: "236px" || prop.height,
+                    backgroundImage: `url(${prop.value.image})`,
+                }}
+                onClick={handleClick}
+            >
+                {dots.map((dot) => (
+                    <div
+                        key={dot.index}
+                        className="captcha-dot"
+                        style={{ top: dot.y - 11, left: dot.x - 11 }}
+                    >
+                        {dot.index}
                     </div>
-                    <div className={"captcha-footer-refresh"}>
-                        <IconFont type={"yg-icon-zhongxinshangchuan"}/>
+                ))}
+            </div>
+            <div className="captcha-footer">
+                <div className="captcha-footer-icon">
+                    <div className="captcha-footer-close">
+                        <IconFont type={"yg-icon-quxiao"} />
+                    </div>
+                    <div className="captcha-footer-refresh">
+                        <IconFont type={"yg-icon-zhongxinshangchuan"} />
                     </div>
                 </div>
-                <Button block type="primary" className={"captcha-footer-btn"}>验证</Button>
+                <Button block type="primary" className="captcha-footer-btn">
+                    验证
+                </Button>
             </div>
         </div>
-    )
-}
+    );
+};
